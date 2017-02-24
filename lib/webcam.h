652214020c018f05d7946966279ebe03ad6fcdb2
Elvisnando <elvis_work@hotmail.it>
2017-02-24 10:46:04 +0100

    prova

--- lib/webcam.h
+++ lib/webcam.h
@@ -1,11 +1,23 @@
 #ifndef WEBCAM_H
 #define WEBCAM_H
 
+#include <libudev.h>
+#include <QtCore/QString>
 
-class Webcam
+#include "device.h"
+#include <libudev.h>
+
+class udev_device;
+
+
+namespace UdevNotifier{
+
+
+
+class Webcam: public Device
 {
 public:
-    Webcam();
+    Webcam(udev_device *device = nullptr);
 };
-
+}
 #endif // WEBCAM_H
