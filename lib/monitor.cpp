7e6c684eaf8fd35e4ef49f752fd6509d8b22f5a2
Elvisnando <elvis_work@hotmail.it>
2017-02-27 12:59:19 +0100

    add commento

--- lib/monitor.cpp
+++ lib/monitor.cpp
@@ -1,12 +1,13 @@
 #include "monitor.h"
 #include "device.h"
-
+#include <QtCore/QDebug>
 
 namespace UdevNotifier {
 
 Monitor::Monitor(udev_device *device)
     :Device(device)
 {
+    qDebug() << "qyesto è un monitr";
 }
 
 Monitor::~Monitor()
